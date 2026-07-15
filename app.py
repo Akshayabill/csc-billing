3ba6c1c6eab49ae8bb991ae3ec5da8cfe49222bb
Akshayabill <jayanjgd2026@gmail.com>
2026-07-15 22:06:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from datetime import datetime
 app = Flask(__name__)
 app.secret_key = "csc_secret"
 
-DATABASE_URL = "postgresql://postgres:CSCbilling2026@db.srtqnjzrgmrqvorhshaq.supabase.co:5432/postgres"
+DATABASE_URL = "postgresql://postgres.srtqnjzrgmrqvorhshaq:Kondazhy106@aws-1-ap-south-1.pooler.supabase.com:6543/postgres"
 db_pool = SimpleConnectionPool(1, 10, dsn=DATABASE_URL)
 
 def get_db_connection():
